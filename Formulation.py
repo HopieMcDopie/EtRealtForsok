6215e345cc83b516bda1d604d24341fb2f625dd0
okvamme <146429210+okvamme@users.noreply.github.com>
2024-11-14 12:00:02 +0100

    vet ikke om det kjører enda...

--- Formulation.py
+++ Formulation.py
@@ -52,8 +52,18 @@ Her må objekticfunskjonen sammen med alle constraintsene være definert først,
 def Obj(m):
     return sum((m.C_spot[t] + m.C_grid[t] )*m.y[t] for t in m.T)
 
-def EnergyBalance(m, t):
-    return m.y[t] == m.D[t] + m.D_EV[t] + m.e_cha[t] - m.e_dis[t]
+
+
+def HouseEnergyBalance(m, t):
+    return m.y_house[t] == m.D[t] + m.e_cha[t] - m.e_dis[t] 
+
+def EVEnergyBalance(m, t):
+    return m.y_EV[t] == m.D_EV[t] + m.e_EV_cha[t] - m.e_EV_dis[t]
+
+def GridImport(m, t):
+    return m.y[t] == m.y_house[t] + m.y_EV[t]
+
+
 
 def SoC(m, t):
     if t == 0:
@@ -69,6 +79,26 @@ def ChargeCap(m, t):
 
 def DischargeCap(m, t):
     return m.e_dis[t] <= m.BatteryDischargeCap
+
+"""
+MÅ LAGE EN B FOR ELBILBATTERIET!! OG FINNE EN INITIAL?
+     
+"""
+
+def SoC_EV(m, t):
+    if t == 0:
+        return m.b_EV[0] == m.e_EV_cha[0]*m.eta - m.e_EV_dis[0]/m.eta 
+    else:
+        return m.b_EV[t] == m.e_EV_cha[t]*m.eta - m.e_EV_dis[t]/m.eta + m.b_EV[t-1]
+    
+def SoCCap_EV(m, t):
+    return m.b_EV[t] <= m.EV_BatteryEnergyCap
+    
+def ChargeCap_EV(m, t):
+    return m.e_EV_cha[t] <= m.EV_BatteryPowerCap[t]
+
+def DischargeCap_EV(m, t):
+    return m.e_EV_dis[t] <= m.EV_BatteryPowerCap[t]
 
 
 def ModelSetUp(SpotPrice, EnergyTariff, Demand, EV_data, constants): #Set up the optimisation model
@@ -88,20 +118,29 @@ def ModelSetUp(SpotPrice, EnergyTariff, Demand, EV_data, constants): #Set up the
     m.BatteryChargeCap    = pyo.Param(initialize = constants['Charge capacity']) #Charging speed/battery power capacity [kW]
     m.BatteryDischargeCap = pyo.Param(initialize = constants['Dishcharge capacity']) #Disharging speed/battery power capacity [kW]
     m.eta                 = pyo.Param(initialize = constants['eta']) #efficiency of charge/discharge
+    m.EV_BatteryEnergyCap = pyo.Param(initialize = constants['EV battery energy capcaity'])
+    m.EV_BatteryPowerCap  = pyo.Param(m.t, initialize = constants['EV battery power capacity'])
     
 
     #Variables
-    m.b     = pyo.Var(m.T, within = pyo.NonNegativeReals) # battery SoC [kWh]
-    m.y     = pyo.Var(m.T, within = pyo.NonNegativeReals) # imported energy from the grid [kWh]
-    m.e_cha = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy charged to the battery [kWh]
-    m.e_dis = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy disherged from the battery [kWh]
+    m.y_house   = pyo.Var(m.T, within = pyo.NonNegativeReals) # imported energy from the grid to the house [kWh]
+    m.y_EV      = pyo.Var(m.T, within = pyo.NonNegativeReals) # imported energy from the grid to the EV [kWh]
+    m.y_total   = pyo.Var(m.T, within = pyo.NonNegativeReals) # imported energy from the grid in total [kWh]
+    m.b         = pyo.Var(m.T, within = pyo.NonNegativeReals) # battery SoC [kWh]
+    m.e_cha     = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy charged to the battery [kWh]
+    m.e_dis     = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy disherged from the battery [kWh]
+    m.b_EV      = pyo.Var(m.T, within = pyo.NonNegativeReals) # EV battery SoC [kWh]
+    m.e_EV_cha  = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy charged to the battery [kWh]
+    m.e_EV_dis  = pyo.Var(m.T, within = pyo.NonNegativeReals) # energy disherged from the battery [kWh]
 
     #Constraints
-    m.EnergyBalance = pyo.Constraint(m.T, rule = EnergyBalance)
-    m.SoC           = pyo.Constraint(m.T, rule = SoC)
-    m.SoCCap        = pyo.Constraint(m.T, rule = SoCCap)
-    m.ChargeCap     = pyo.Constraint(m.T, rule = ChargeCap) 
-    m.DischargeCap  = pyo.Constraint(m.T, rule = DischargeCap)
+    m.HouseEnergyBalance    = pyo.Constraint(m.T, rule = HouseEnergyBalance)
+    m.EVEnergyBalance       = pyo.Constraint(m.T, rule = EVEnergyBalance)
+    m.GridImport            = pyo.Constraint(m.T, rule = GridImport)
+    m.SoC                   = pyo.Constraint(m.T, rule = SoC)
+    m.SoCCap                = pyo.Constraint(m.T, rule = SoCCap)
+    m.ChargeCap             = pyo.Constraint(m.T, rule = ChargeCap) 
+    m.DischargeCap          = pyo.Constraint(m.T, rule = DischargeCap)
 
     #Objective function
     m.Obj = pyo.Objective(rule = Obj, sense = pyo.minimize)
@@ -129,9 +168,9 @@ def Graphical_results(m):
         hours.append(t)
         demand.append(m.D[t])
         EV_demand.append(m.D_EV[t])
-        price.append(m.C[t])
+        price.append(m.C_spot[t])
         battery.append(m.b[t].value)
-        y.append(m.y[t].value)
+        y.append(m.y_total[t].value)
 
 
     fig, ax1 = plt.subplots()
